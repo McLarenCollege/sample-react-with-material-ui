08aef1c5d2fc441751db89319fe02c505fc6be3b
McLarenCollege <mark.k.mclaren@gmail.com>
2020-04-20 01:42:21 +1000

    Initial commit

---
+++ precache-manifest.aa5181af7621151b75e6453a2c18e1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74bf1c4aec99899d97d4a9a0cdf05446",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03c979b082f1c27c7047",
+    "url": "/static/css/main.eae67742.chunk.css"
+  },
+  {
+    "revision": "929a865e18d3341c0084",
+    "url": "/static/js/2.144cb564.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.144cb564.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c979b082f1c27c7047",
+    "url": "/static/js/main.8388c8d2.chunk.js"
+  },
+  {
+    "revision": "8fc247570abc274ed64f",
+    "url": "/static/js/runtime-main.85ec29e2.js"
+  }
+]);
